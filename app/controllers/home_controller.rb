29b86fea4368f8b173aff9b7947ebaceda774446
yusuke-kawara <happywanwan2007@gmail.com>
2024-05-03 22:42:54 +0900

    disricord 機能実装完了
    一回デプロイして動作を確認する。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def random_audio
+    audios = Dir.glob("app/assets/audios/*.mp3")
+    random_audio = audios.sample.split("/").last
+    render json: { audio: random_audio }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  get 'home/random_audio', to: 'home#random_audio'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
