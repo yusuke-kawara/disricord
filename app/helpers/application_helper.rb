360bcad0f624a3fcd6bc73336d7129fbf8be2d29
yusuke-kawara <happywanwan2007@gmail.com>
2024-05-04 03:16:05 +0900

    disricord 機能実装完了6
    ogイメージが作成されない。gem meta-tagsを入れてデプロイして動作を確認してみる。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
-  def og_tags(title: "ディスリコード", description: "ただひたすらディスられるだけのアプリ！！", image: "itazura240.jpg")
-    tag.meta(property: "og:title", content: title) +
-    tag.meta(property: "og:description", content: description) +
-    tag.meta(property: "og:image", content: image_url(image))
+  def default_meta_tags(title: "ディスリコード", description: "ただひたすらディスられるだけのアプリ！！", image: "itazura240.jpg")
+    set_meta_tags og: {
+      title:       title,
+      description: description,
+      image:       image_url(image)
+    }
   end
 end
