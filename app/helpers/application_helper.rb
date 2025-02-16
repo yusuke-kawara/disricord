b2fec4611305a0087a42d757fc7a896c764278c9
yusuke-kawara <happywanwan2007@gmail.com>
2025-02-17 03:45:26 +0900

    ブログにリンクを貼った際URLがそのまま表示されてしまう問題が発生 3
    application.html.erbの<!-- Open Graph Protocol for better link preview -->の設定にミスがないかチェック中です。
    Xの共有では正常に表示されているためここにタイポかなにかがあって正常に表示されないのではと仮説をたてました。
    
    application.html.erbのhead内に下記を追記
    <%= display_meta_tags(default_meta_tags) %>
    </head>
    
    application_helper.rbに値を直接参照するように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,23 @@
 module ApplicationHelper
-  def default_meta_tags(title: "ディスリコード", description: "ただひたすらディスられるだけのアプリ！！", image: "itazura240.jpg")
-    set_meta_tags og: {
-      title:       title,
-      description: description,
-      image:       image_url(image)
+  def default_meta_tags
+    {
+      site: 'ディスリコード',
+      title: 'ディスリコード',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ただディスられるだけのアプリ。',
+      keywords: 'ディスリコード',
+      canonical: request.try(:original_url),
+      separator: '|',
+      og: {
+        site_name: 'ディスリコード',
+        title: 'ディスリコード',
+        description: 'ただディスられるだけのアプリ。',
+        type: 'website',
+        url: request.try(:original_url),
+        image: image_url('itazura240.jpg'),
+        local: 'ja-JP'
+      },
     }
   end
 end
